4d32a7253778f13666eb445971f45d347a39919a
mercy-mwai <mercyneemam@gmail.com>
2025-04-11 04:55:39 +0300

    installed next auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+"use client"
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
 
 const Nav = () => {
   return (
