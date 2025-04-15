2bf39b13fb66243cc00a104e92b9c16b932fab0b
mercy-mwai <mercyneemam@gmail.com>
2025-04-15 19:53:23 +0300

    weeeh😒😑

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,18 @@
-"use client"
-import Nav from '@/components/Nav'
+
 
 const Home = () => {
   return (
-    <section className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-screen  ">
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-      <div className="relative z-10 text-white p-10"> </div>
-      <div className="relative z-10 text-white p-10">
-        <h1 className="text-4xl font-bold">Hi, I'm Njoki — Welcome to my Portfolio</h1>
-        <p className="mt-4 text-lg">Software Developer | Lifelong Learner</p>
-      </div>
-    </section>
-    
+    // <section className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-screen  ">
+    //   <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+    //   <div className="relative z-10 text-white p-10"> </div>
+    //   <div className="relative z-10 text-white p-10">
+    //     <h1 className="text-4xl font-bold">Hi, I'm Njoki — Welcome to my Portfolio</h1>
+    //     <p className="mt-4 text-lg">Software Developer | Lifelong Learner</p>
+    //   </div>
+    // </section>
+    <div className="bg-red-500 p-10 text-white">
+   Hello Tailwind!
+  </div>
   )
 }
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,36 +8,47 @@ const Nav = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="flex justify-between items-center w-full mb-16 pt-3">
-      {/** desktop Navigation*/}
-      <div className="sm:hidden flex">
-      <div className='flex gap-3 md:gap-7'>
-        <Link href='/' className={`black_btn ${pathname === '/' ? 'active' : ''}`}>Home</Link>
-        <Link href='/about' className={`black_btn ${pathname === '/about' ? 'active' : ''}`}>About</Link>
-        <Link href='/projects' className={`black_btn ${pathname === '/projects' ? 'active' : ''}`}>Projects</Link>
-        <Link href='/contact' className={`black_btn ${pathname === '/contact' ? 'active' : ''}`}>Contact</Link>
-      </div>
-      </div>
-      {/* Mobile Toggle Button */}
-      <div className="sm:hidden flex items-center">
-        <button
-          onClick={() => setToggleDropdown(!toggleDropdown)}
-          className="text-white focus:outline-none"
-        >
-          ☰
-        </button>
-      </div>
+    // <nav className="flex justify-between items-center w-full mb-16 pt-3">
+    //   {/** desktop Navigation*/}
+    //   <div className="sm:hidden flex">
+    //   <div className='flex gap-3 md:gap-7'>
+    //     <Link href='/' className={`black_btn ${pathname === '/' ? 'active' : ''}`}>Home</Link>
+    //     <Link href='/about' className={`black_btn ${pathname === '/about' ? 'active' : ''}`}>About</Link>
+    //     <Link href='/projects' className={`black_btn ${pathname === '/projects' ? 'active' : ''}`}>Projects</Link>
+    //     <Link href='/contact' className={`black_btn ${pathname === '/contact' ? 'active' : ''}`}>Contact</Link>
+    //   </div>
+    //   </div>
+    //   {/* Mobile Toggle Button */}
+    //   <div className="sm:hidden flex items-center">
+    //     <button
+    //       onClick={() => setToggleDropdown(!toggleDropdown)}
+    //       className="text-white focus:outline-none"
+    //     >
+    //       ☰
+    //     </button>
+    //   </div>
 
-      {/* Mobile Dropdown Menu */}
-      {toggleDropdown && (
-        <div className="absolute top-16 right-5 bg-gray-800 rounded-lg p-4 flex flex-col gap-3 z-50 sm:hidden">
-          <Link href='/' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/' ? 'font-bold' : ''}`}>Home</Link>
-          <Link href='/about' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/about' ? 'font-bold' : ''}`}>About</Link>
-          <Link href='/projects' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/projects' ? 'font-bold' : ''}`}>Projects</Link>
-          <Link href='/contact' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/contact' ? 'font-bold' : ''}`}>Contact</Link>
+    //   {/* Mobile Dropdown Menu */}
+    //   {toggleDropdown && (
+    //     <div className="absolute top-16 right-5 bg-gray-800 rounded-lg p-4 flex flex-col gap-3 z-50 sm:hidden">
+    //       <Link href='/' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/' ? 'font-bold' : ''}`}>Home</Link>
+    //       <Link href='/about' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/about' ? 'font-bold' : ''}`}>About</Link>
+    //       <Link href='/projects' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/projects' ? 'font-bold' : ''}`}>Projects</Link>
+    //       <Link href='/contact' onClick={() => setToggleDropdown(false)} className={`text-white ${pathname === '/contact' ? 'font-bold' : ''}`}>Contact</Link>
+    //     </div>
+    //   )}
+
+    // </nav>
+    <nav className="bg-black">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
+            <Link href="/" className="text-white">Home</Link>
+            </div>
         </div>
-      )}
-
+        </div>
+      </div>
     </nav>
   )
 }
