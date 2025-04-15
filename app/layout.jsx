cffd4b086009cdd1d727c17ad5f90cde4bb8dfd5
mercy-mwai <mercyneemam@gmail.com>
2025-04-15 15:29:52 +0300

    updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,20 @@
-import Nav from "@/components/Nav";
 import "../styles/global.css";
+import Nav from "@/components/Nav";
 
+export const metadata = {
+  title: "Njoki's Portfolio",
+  description: "Welcome to my portfolio!",
+};
 
 const RootLayout = ({ children }) => {
     return (
       <html lang="en">
-        <body>
-          <div>
-            <div  />
-          </div>
-          <main >
-            {children}</main>
-        </body>
-      </html>
+      <body>
+        <main>
+        <Nav />
+          {children}</main>
+      </body>
+    </html>
     );
   };
   
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Nav from '@/components/Nav'
 
 const Home = () => {
@@ -5,10 +6,9 @@ const Home = () => {
     <section className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-screen  ">
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       <div className="relative z-10 text-white p-10"> </div>
-      <Nav />
       <div className="relative z-10 text-white p-10">
         <h1 className="text-4xl font-bold">Hi, I'm Njoki — Welcome to my Portfolio</h1>
-        <p className="mt-4 text-lg">Frontend Developer | Lifelong Learner</p>
+        <p className="mt-4 text-lg">Software Developer | Lifelong Learner</p>
       </div>
     </section>
     
