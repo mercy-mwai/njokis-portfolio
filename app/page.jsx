b62c17ef3fa9c9d4d856eb2bbab40adaee4facc4
mercy-mwai <mercyneemam@gmail.com>
2025-04-15 14:31:14 +0300

    updates

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
 
-const page = () => {
+
+const Home = () => {
   return (
-    <div>page</div>
+    <section className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-screen  ">
+      <div className="w-full h-screen flex items-end"></div>
+      <div className="relative z-10 text-white p-10">
+      <h1>Hi, I'm Njoki — Welcome to my Portfolio</h1>
+    </div>
+    </section>
+    
   )
 }
 
-export default page
+export default Home
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import { usePathname } from "next/navigation";
 
 const Nav = () => {
