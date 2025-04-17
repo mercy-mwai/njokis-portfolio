8eacb67e320d070839006a1b772c0e1fe5ea9e93
mercy-mwai <mercyneemam@gmail.com>
2025-04-17 19:26:57 +0300

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,32 @@
+"use client";
+import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
   return (
     <div className="h-screen overflow-y-scroll">
-  <section className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-[300vh] relative w-full">
-    {/* Content */}
-    <div className="relative z-20 text-white p-20">
-      <h1 className="text-4xl font-bold m-10">Hi there👋🏽, 
-        <span>I'm Njoki — Welcome to my Portfolio</span></h1>
-      <p className="mt-4 text-lg">Software Developer | Lifelong Learner</p>
+      <section className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-[250vh] relative w-full">
+        {/* Content */}
+
+        <div className="relative z-20 text-white pt-60 px-16">
+          <h1 className="text-[2.4em] font-bold mb-6 text-left pl-14">Hi there
+            <span role="img" aria-label="waving hand">👋🏽</span>
+          </h1>
+          <h1 className="text-[2.5em] pl-14">I'M <strong className="main-name text-purple-300">NJOKI MWAI</strong></h1>
+          <p className="mt-4 text-[2.0em] text-left pl-14  text-purple-300">
+          <Typewriter
+          words={["Software Developer 👩🏽‍💻", "| Lifelong Learner 🚀","Welcome to my portfolio ✨", "Let's build something amazing 🚀" ]}
+          loop={0}
+          cursor
+          cursorStyle="_"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+           </p>
+        </div>
+      </section>
     </div>
-  </section>
-</div>
-  )
-}
+  );
+};
 
 export default Home;
-
