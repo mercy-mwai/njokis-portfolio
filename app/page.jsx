b1c46b3e5cdc44571fa1c36fa467e7c245132cfc
mercy-mwai <mercyneemam@gmail.com>
2025-04-17 18:30:32 +0300

    installed framer-motion for animations

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,8 @@ const Home = () => {
   <section className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-[300vh] relative w-full">
     {/* Content */}
     <div className="relative z-20 text-white p-20">
-      <h1 className="text-4xl font-bold">Hi, I'm Njoki — Welcome to my Portfolio</h1>
+      <h1 className="text-4xl font-bold m-10">Hi there👋🏽, 
+        <span>I'm Njoki — Welcome to my Portfolio</span></h1>
       <p className="mt-4 text-lg">Software Developer | Lifelong Learner</p>
     </div>
   </section>
