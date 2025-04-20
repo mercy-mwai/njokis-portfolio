37c227a59fc0b8b626e697b236fb2f7851254254
mercy-mwai <mercyneemam@gmail.com>
2025-04-20 19:43:05 +0300

    commit message

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,7 +5,6 @@ const Banner = () => {
       <div
         className="bg-[url('/assets/images/banner.png')] bg-cover bg-center min-h-[250vh] w-full relative"
       >
-        {/* Optional content inside banner */}
       </div>
     );
   };
