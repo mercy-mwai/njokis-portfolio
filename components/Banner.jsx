e5eeefc4ab8183385e9214a90700567dd2766e1e
mercy-mwai <mercyneemam@gmail.com>
2025-04-28 14:56:15 +0300

    updates

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,7 +2,7 @@
 
 const Banner = ({children}) => {
     return (
-      <div className="bg-[url('/assets/images/CI.png')] bg-cover bg-center bg-no-repeat bg-fixed min-h-[215vh] w-full relative">
+      <div className="bg-[url('/assets/images/CI.png')] bg-cover bg-center bg-no-repeat bg-fixed min-h-[185vh] w-full relative">
         {children}
       </div>
     );
