00f43067b8140b8a8d5f0ea2058bc0cbd314be44
mercy-mwai <mercyneemam@gmail.com>
2025-05-03 13:26:44 +0300

    commit message

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -1,22 +1,20 @@
-// import { PrismaClient } from '@prisma/client';
+import { prisma} from '@/app/utils/prisma';
 
 
-// const prisma = new PrismaClient();
-
-// export async function POST(request){
-//     const {name, email, content}=await request.json();
-//     console.log(name, email, content);
-//     if(!name || !email || !content){
-//         return new Response('Missing fields', {status: 400});
-//     }
-//     try{
-//         const message = await prisma.message.create({
-//             data: { name, email, content },
-//           });
+export async function POST(request){
+    const {name, email, content}=await request.json();
+    console.log(name, email, content);
+    if(!name || !email || !content){
+        return new Response('Missing fields', {status: 400});
+    }
+    try{
+        const message = await prisma.message.create({
+            data: { name, email, content },
+          });
       
-//           return new Response(JSON.stringify(message), { status: 200 });
-//     }catch(error){
-//         console.error('Error saving message:', error);
-//         return new Response('Internal Server Error', {status: 500});
-//     }
-// }
+          return new Response(JSON.stringify(message), { status: 200 });
+    }catch(error){
+        console.error('Error saving message:', error);
+        return new Response('Internal Server Error', {status: 500});
+    }
+}
