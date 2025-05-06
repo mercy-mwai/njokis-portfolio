c8eeefc10502d653967c185700ab6d2c7e079d13
mercy-mwai <mercyneemam@gmail.com>
2025-05-06 17:04:18 +0300

    updates

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -1,20 +1,24 @@
-export const runtime = 'nodejs';
-import { prisma} from '@/app/utils/prisma';
+import pool from '../../lib/db' 
 
-export async function POST(request){
-    const {name, email, content}=await request.json();
-    console.log(name, email, content);
-    if(!name || !email || !content){
-        return new Response('Missing fields', {status: 400});
-    }
-    try{
-        const message = await prisma.message.create({
-            data: { name, email, content },
-          });
-      
-          return new Response(JSON.stringify(message), { status: 200 });
-    }catch(error){
-        console.error('Error saving message:', error);
-        return new Response('Internal Server Error', {status: 500});
-    }
+export async function POST(request) {
+  const { name, email, content } = await request.json();
+
+  console.log(name, email, content);
+  if (!name || !email || !content) {
+    return new Response("Missing fields", { status: 400 });
+  }
+  try {
+    const query = `
+      INSERT INTO message (name, email, content)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const values = [name, email, content];
+    const result = await pool.query(query, values);
+
+    return new Response(JSON.stringify(result.rows[0]), { status: 200 });
+  } catch (error) {
+    console.error("DB error:", error);
+    return new Response("Database Error", { status: 500 });
+  }
 }
